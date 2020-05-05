896feeb2386aff7ace2ee8f732a7ae3e01292f59
Nkateko00 <tekonku@icloud.com>
2020-05-05 13:30:39 +0200

    my changes

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -6,7 +6,7 @@ const criticalLevelElement = document.querySelector(".criticalLevel");
 //references to the Totals
 const callTotalElement = document.querySelector(".callTotalSettings");
 const smsTotalElement = document.querySelector(".smsTotalsSettings");
-const totalElement =document.querySelector("totalSettings");
+const totalElement = document.querySelector("totalSettings");
 //get a reference to the add button
 const myAddBtn = document.querySelector(".addBillBtn");
 //get a reference to the 'Update settings' button
@@ -14,61 +14,56 @@ const myUpdateSettingsBtn = document.querySelector(".updateSettings");
 // create a variables that will keep track of all the settings
 var totalCostSettings = 0;
 var totalSmsSetting = 0;
-var totalWarningLevel =0;
-var totalCriticalLevel =0;
+var totalWarningLevel = 0;
+var totalCriticalLevel = 0;
 // create a variables that will keep track of all three totals.
 var callTotals = 0;
-var smsTotals = 0
-var totalSetting =0;
-function settingsBill(){
-    totalCostSettings = Number(callCostSettingsElement.value);
-    totalSmsSetting =  Number(smsCostSettingsElement.value);
-    totalWarningLevel= Number(warningLevelElement.value);
+var smsTotals = 0;
+var totalSetting = 0;
+function settingsBill() {
+    totalCostSettings =  Number(callCostSettingsElement.value);
+    totalSmsSetting = Number(smsCostSettingsElement.value);
+    totalWarningLevel = Number(warningLevelElement.value);
     totalCriticalLevel = Number(criticalLevelElement.value);
-
-    forColor(totalSettings, totalWarningLevel, totalCriticalLevel);
-    
+    forColor(totalSetting, totalWarningLevel, totalCriticalLevel);
 }
 //add an event listener for when the 'Update settings' button is pressed
 
-function totalSettings(){
-    var billTypeRadioCheck = document.querySelector("input[name ='billItemTypeWithSettings']:checked");    
-    var getBillType = billTypeRadioCheck.value;
-    if(getBillType==="call"){
-        callTotals += totalCostSettings;
-        totalSetting+= totalCostSettings;
-    }
-    else if(getBillType === "sms"){
-        smsTotals += totalSmsSetting;
-        totalSetting += totalSmsSetting;
-    }
-    innerScript();
-}
-function forColor(fullTotal,latestWarning,latestDanger){
-    totalElement.classList.remove("danger");
-    totalElement.classList.remove("warning");
-    if(fullTotal >= latestWarning && fullTotal<latestDanger){
-        totalElement.classList.add("warning");
-    }else if(fullTotal>=latestDanger){
-        totalElement.classList.add("danger");
+function totalSettings() {
+    var billTypeRadioCheck = document.querySelector("input[name ='billItemTypeWithSettings']:checked");
+    if (billTypeRadioCheck) {
+        var getBillType = billTypeRadioCheck.value;
+        if (totalSetting < totalCriticalLevel) {
+            if (getBillType === "call") {
+                callTotals += totalCostSettings;
+                totalSetting += totalCostSettings;
+            }
+        }
+        else if (getBillType === "sms") {
+            smsTotals += totalSmsSetting;
+            totalSetting += totalSmsSetting;
+        }
+        innerScript();
+        forColor(totalSetting, totalWarningLevel, totalCriticalLevel);
+
     }
 }
+    function forColor(fullTotal, latestWarning, latestDanger) {
+        totalElement.classList.remove("danger");
+        totalElement.classList.remove("warning");
+        if (fullTotal >= latestWarning && fullTotal < latestDanger) {
+            totalElement.classList.add("warning");
+        }
+        else if (fullTotal >= latestDanger) {
+            totalElement.classList.add("danger");
+        }
+    }
 
-function innerScript(){
-    forColor(totalSettings, totalWarningLevel, totalCriticalLevel);
-    callTotalElement.innerHTML = callTotals.toFixed(2);
-    smsTotalElement.innerHTML = smsTotals.toFixed(2);
-    totalElement.innerHTML = totalSetting.toFixed(2);
+    function innerScript() {
+        callTotalElement.innerHTML = callTotals.toFixed(2);
+        smsTotalElement.innerHTML = smsTotals.toFixed(2);
+        totalElement.innerHTML = totalSetting.toFixed(2);
 
-}
-myAddBtn.addEventListener("click",totalSettings);
-myUpdateSettingsBtn.addEventListener("click",settingsBill);
-
-//add an event listener for when the add button is pressed
-
-
-// * add the appropriate value to the call / sms total
-// * add the appropriate value to the overall total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right color.
+    }
+    myAddBtn.addEventListener("click", totalSettings);
+    myUpdateSettingsBtn.addEventListener("click", settingsBill);

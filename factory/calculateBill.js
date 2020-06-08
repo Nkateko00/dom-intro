a4bdc2c987f72700ec473a278f41f53190a64acb
Nkateko00 <tekonku@icloud.com>
2020-06-08 14:22:14 +0200

    changes

--- factory/calculateBill.js
+++ factory/calculateBill.js
@@ -1,22 +1,24 @@
  function billCalculation(){
  function calculateBill(billStringValue){
     var billList = billStringValue.split(",");
-    totalBill=0;
+    fullBill=0;
     //  * loop over all the entries in the the resulting list
     for(var i =0;i<billList.length;i++){
          var bill = billList[i].trim();
          if(bill==="call"){
-             totalBill+=2.75;
+             fullBill+=2.75;
          }
          else if(bill==="sms"){
-             totalBill+=0.75;
-         } 
-        //  console.log(totalBill); 
-     }
-     return totalBill;
-}
-return { 
-    calculateBill
+             fullBill+=0.75;
+            }
 }
  }
-
+function getTotal(){
+    return fullBill
+    //create a function which returns the total
+}
+return { 
+    calculateBill,getTotal
+}
+ }
+ 
--- test/calculateBill.test.js
+++ test/calculateBill.test.js
@@ -1,10 +1,19 @@
 describe("using the calculate Bill Wigdet",function(){
-    it("It should return the correct amount for the specified amount of calls(2) at R3.50 each",function(){
-            var total = calculateBill();
-            assert.equal(4.25,total);
+    it("It should return the correct R5.50 for two calls made",function(){
+        var  total = billCalculation();
+        total.calculateBill("call,call");
+         assert.equal(5.50,total.getTotal());
         });
- 
+        it("It should return the correct R2.75 for 3 sms & 2 calls made",function(){
+            var  total = billCalculation();
+            total.calculateBill("call,sms,call,sms,sms");
+             assert.equal(7.75,total.getTotal());
+            });
+      it("It should return the correct R2.75 for 3 sms & 2 calls made",function(){
+            var  total = billCalculation();
+            total.calculateBill("call,sms,call,sms,sms");
+             assert.equal(7.75,total.getTotal());
     });
 
-
-
+    
+});

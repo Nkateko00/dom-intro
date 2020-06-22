8774c0f85986f993afd0583777105a853a30665c
Nkateko00 <tekonku@icloud.com>
2020-06-22 15:02:54 +0200

    changes

--- factory/calculateBill.js
+++ factory/calculateBill.js
@@ -1,12 +1,6 @@
- 
- function billCalculation(){
-    var emptyString = "";
-    var fullBill;
-    var theWarningLevel = 20;
-    var theCriticalLevel = 30;
-function calculateTheBill(billStringValue){
+ function calculateTheBill(billStringValue){
    var billList = billStringValue.split(",");
-    fullBill = 0;
+    var fullBill = 0;
    //  * loop over all the entries in the the resulting list
    for(var i =0;i<billList.length;i++){
         var bill = billList[i].trim();
@@ -17,27 +11,6 @@ function calculateTheBill(billStringValue){
             fullBill+=0.75;
            }
 }
-}
-function setString(item){
-   emptyString = item ;
-
-}
-function testLevel(){
-    if(fullBill>=theWarningLevel && fullBill<CriticalLevel){
-       return "warning";
-    }else if(fullBill>=theCriticalLevel){
-       return "danger";
-    }
-}
-function getTotal(){
-   return fullBill
-   //create a function which returns the total
-}
-return { 
-   calculateTheBill,
-   testLevel,
-   setString,
-   getTotal
-}
+return fullBill
 }
  
--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -3,25 +3,29 @@ const calculateBtnElement = document.querySelector(".calculateBtn");
 const billTotalElement = document.querySelector(".billTotal");
 //get a reference to the billString
 const billStringElement = document.querySelector(".billString");
+//This is the element used to store the total,most important one too
 const billTotalElementTime= document.querySelector(".total");
 //create the function that will be called when the calculate button is pressed
-var totalBill = billCalculation()
-function  calculateBill(){
-    totalBill.setString(billStringElement.value);
-    innerText();
+function billCalculation(){
     showColors();
+    billTotalElementTime.innerHTML = calculateTheBill(billStringElement.value).toFixed(2);
+
 }
+
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 function showColors() {
-    billTotalElement.classList.remove("danger");
-    billTotalElement.classList.remove("warning");
-    billTotalElement.classList.add(totalBill.testLevel());
-    //console.log(totalBill);
-}
+    billTotalElementTime.classList.remove("danger");
+    billTotalElementTime.classList.remove("warning");
+   
+    if(calculateTheBill(billStringElement.value)>=30){
+       billTotalElementTime.classList.remove("warning");
+      billTotalElementTime.classList.add("danger");
+   }
+    if(calculateTheBill(billStringElement.value) >=20 && calculateTheBill(billStringElement.value)< 30){
+       billTotalElementTime.classList.remove("danger");
+       billTotalElementTime.classList.add("warning");
+    }
+    }
+    calculateBtnElement.addEventListener("click", billCalculation);
 
-function innerText() {
-    billTotalElementTime.innerHTML = (totalBill.getTotal()).toFixed(2);
- 
-}
-calculateBtnElement.addEventListener("click", calculateBill());
